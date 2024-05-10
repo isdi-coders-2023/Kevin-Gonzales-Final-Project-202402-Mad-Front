9846d1b148d899fef929eebd1df175a5f95e3763
Kev <kev@Air-de-Kevin.localdomain>
2024-05-10 11:24:58 +0200

    components created

--- src/app/services/state/state.service.ts
+++ src/app/services/state/state.service.ts
@@ -8,77 +8,86 @@ import { UsersService } from '../users/users.service';
 type LoginState = 'idle' | 'register' | 'logged' | 'error';
 
 export type Payload = {
-  id: string;
-  role: string;
+ id: string;
+ role: string;
 } & JwtPayload;
 
 export type State = {
-  loginState: LoginState;
-  token: string | null;
-  currenPayload: Payload | null;
-  currenUser: unknown | null;
-  clubs: Club[];
+ loginState: LoginState;
+ token: string | null;
+ currenPayload: Payload | null;
+ currenUser: unknown | null;
+ clubs: Club[];
 };
 
 const initialState: State = {
-  loginState: 'idle',
-  token: null,
-  currenPayload: null,
-  currenUser: null,
-  clubs: [],
+ loginState: 'idle',
+ token: null,
+ currenPayload: null,
+ currenUser: null,
+ clubs: [],
 };
 
 @Injectable({
-  providedIn: 'root',
+ providedIn: 'root',
 })
 export class StateService {
-  public state$ = new BehaviorSubject<State>(initialState);
-  private clubsService = inject(ClubsService);
-  private userService = inject(UsersService);
-  jwtDecode = jwtDecode;
+ public state$ = new BehaviorSubject<State>(initialState);
+ private clubsService = inject(ClubsService);
+ private userService = inject(UsersService);
+ jwtDecode = jwtDecode;
 
-  getState(): Observable<State> {
-    return this.state$.asObservable();
-  }
+ getState(): Observable<State> {
+  return this.state$.asObservable();
+ }
 
-  get state(): State {
-    return this.state$.value;
-  }
+ get state(): State {
+  return this.state$.value;
+ }
 
-  setLoginState(loginState: LoginState) {
-    this.state$.next({ ...this.state$.value, loginState });
-  }
+ setLoginState(loginState: LoginState) {
+  this.state$.next({ ...this.state$.value, loginState });
+ }
 
-  setRegister() {
-    this.state$.next({
-      ...this.state$.value,
-      loginState: 'register',
-      token: null,
-      currenPayload: null,
-    });
-  }
+ setRegisterForm() {
+  this.state$.next({
+   ...this.state$.value,
+   loginState: 'register',
+   token: null,
+   currenPayload: null,
+  });
+ }
 
-  setLogin(token: string) {
-    const currenPayload = this.jwtDecode(token) as Payload;
-    localStorage.setItem('Y dale U', JSON.stringify({ token }));
-    this.userService.getById(currenPayload.id).subscribe((user) => {
-      this.state$.next({
-        ...this.state$.value,
-        loginState: 'logged',
-        token,
-        currenPayload,
-        currenUser: user,
-      });
-    });
-  }
+ setLoginForm() {
+  this.state$.next({
+   ...this.state$.value,
+   loginState: 'idle',
+   token: null,
+   currenPayload: null,
+  });
+ }
 
-  setLogout() {
-    localStorage.removeItem('Y dale U');
-    this.state$.next({
-      ...this.state$.value,
-      loginState: 'idle',
-      token: null,
-      currenPayload: null,
-    });
-  }
+ setLogin(token: string) {
+  const currenPayload = this.jwtDecode(token) as Payload;
+  localStorage.setItem('Y dale U', JSON.stringify({ token }));
+  this.userService.getById(currenPayload.id).subscribe((user) => {
+   this.state$.next({
+    ...this.state$.value,
+    loginState: 'logged',
+    token,
+    currenPayload,
+    currenUser: user,
+   });
+  });
+ }
+
+ setLogout() {
+  localStorage.removeItem('Y dale U');
+  this.state$.next({
+   ...this.state$.value,
+   loginState: 'idle',
+   token: null,
+   currenPayload: null,
+  });
+ }
 }
